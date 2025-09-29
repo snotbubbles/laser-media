7949370b44b6989f29f9ef01d34153d8462be72a
snotbubbles <47840888+snotbubbles@users.noreply.github.com>
2025-09-29 11:08:35 +0200

    Bootstrap js custom v1.0

---
+++ bootstrap.custom.js
@@ -0,0 +1,6 @@
+// Import Carousel
+import 'bootstrap/js/dist/carousel';
+
+// Expose globally (so your index.js works as written)
+import * as bootstrap from 'bootstrap';
+window.bootstrap = bootstrap;
