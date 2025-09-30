a0fbf1067409a012c9fa3db850e8db3500f77791
snotbubbles <47840888+snotbubbles@users.noreply.github.com>
2025-09-30 14:24:51 +0200

    fontawesome.purged.css

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,26 +1,24 @@
 const autoprefixer = require('autoprefixer');
 const purgecss = require('@fullhuman/postcss-purgecss').default;
+const postcssImport = require('postcss-import');
 
 module.exports = {
   plugins: [
+    postcssImport(),
     autoprefixer(),
     purgecss({
       content: ['./**/*.html', './**/*.js'],
       defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
       safelist: [
-        // Carousel
-        /^carousel/,
-        'active',
-        'show',
-        'fade',
+        // Bootstrap
+        /^carousel/, 'active', 'show', 'fade',
+        /^collapse/, /^collapsing/,
+        'accordion-button', 'accordion-collapse',
+        'accordion-body', 'accordion-item',
 
-        // Accordion / Collapse
-        /^collapse/,
-        /^collapsing/,
-        'accordion-button',
-        'accordion-collapse',
-        'accordion-body',
-        'accordion-item'
+        // Font Awesome
+        /^fa-solid$/, 
+        /^fa-triangle-exclamation$/
       ]
     })
   ]
